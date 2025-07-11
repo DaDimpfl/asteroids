0ecc2a3e999014afff7cc3295c3ce45a5b609f47
DaDimpfl <damien.dimpfl@web.de>
2025-07-11 11:24:53 +0200

    groups!!!!

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     framerate = 60
@@ -13,17 +16,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     #Game Loop
-    counter = 0
-    while counter == 0:
+    while True:
         #Check if game is closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-
-        player.draw(screen)
-        player.update(dt)
-
+        updatable.update(dt)
+        screen.fill("black")        
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         #Limit Framerate
         dt = clock.tick(framerate) / 1000
